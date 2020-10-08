954cabfcfabbe4ecc297c0372718e375eb7e74e3
ariss95 <cse32538@cs.uoi.gr>
2020-10-09 02:24:58 +0300

    scheduled to run every 2 hours

--- notifier.py
+++ notifier.py
@@ -1,8 +1,7 @@
 import notify2
 from bs4 import BeautifulSoup as soup
 from requests_html import HTMLSession
-
-s = HTMLSession()
+import schedule, time
 
 def get_items():
     file1 = open("wishlist.txt", "r")
@@ -14,7 +13,7 @@ def check_for_discount():
     for item in items:
         item = item.strip("\n") #remove newline character from the url
         
-        doc = s.get(item).text #get html document
+        doc = session.get(item).text #get html document
         html = soup(doc, "html.parser")
         special_price = html.findAll("div", {"class": "e-product-price__special"})
         if special_price != []:
@@ -23,8 +22,13 @@ def check_for_discount():
             n.show()
         else:
             print("no discount")
-
+interval = 120 #minutes, change value for shorter time between notifications
+session = HTMLSession()
 notify2.init("discount notifier")
 n = notify2.Notification("", "")
 
 check_for_discount()
+schedule.every(interval).minutes.do(check_for_discount)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
